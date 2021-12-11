878bcb2872438d941bee690b5ffcb6ea7c33666e
DESKTOP-FV08FA3\Rima <danapradana11@gmail.com>
2021-12-12 02:48:19 +0700

    Scene lemon update

--- Assets/Scripts/Script Lemon/TeleportPosition.cs
+++ Assets/Scripts/Script Lemon/TeleportPosition.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class TeleportPosition : MonoBehaviour
 {
     [SerializeField] private Transform destination;
-    
-    public Transform getDestination()
-    {
-        return destination;
-    }
+    public Rigidbody2D rb;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = destination.position;
+            collision.transform.position = destination.transform.position;
+            Teleport(collision);
         }
     }
+
+    private void Start()
+    {
+        rb = rb.GetComponent<Rigidbody2D>();
+    }
+    private void Teleport(Collider2D other)
+    {
+        rb.MovePosition(other.transform.position);
+        
+    }
+
 
 }
