9fed6ad97c91c1db2605a40f658af70ce31969f1
DESKTOP-FV08FA3\Rima <danapradana11@gmail.com>
2021-12-11 17:47:20 +0700

    tilemap lemon complete

--- Assets/Scripts/Script Lemon/TeleportPosition.cs
+++ Assets/Scripts/Script Lemon/TeleportPosition.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TeleportPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform destination;
+    
+    public Transform getDestination()
     {
-        
+        return destination;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = destination.position;
+        }
     }
+
 }
